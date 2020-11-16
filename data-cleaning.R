d8acdb5c5380c60ad0bec7cb8e044bb92471ec40
ncstabile17 <ncstabile17@gmail.com>
2020-11-16 01:31:29 -0500

    Initial setup, reading in all permit years, filtering by new building

---
+++ data-cleaning.R
@@ -0,0 +1,36 @@
+library(tidyverse)
+
+# I want to import permit data from various years so I can merge them and look at them together
+
+all_permits <- data.frame()
+
+append_permit_data <- function(.year) {
+  
+  file_name <- str_glue(
+    "data/Building_Permits_in_{.year}.csv"
+  )
+  
+  new_permit_data <- 
+    read_csv(file_name, col_types = cols(
+      PERMIT_CATEGORY_NAME = col_character()
+    )) %>% 
+    rename_with(tolower) %>%
+    filter(permit_subtype_name == "NEW BUILDING") %>% 
+    select(-permit_category_name)
+  
+  all_permits <- bind_rows(
+    all_permits,
+    new_permit_data
+  )
+  
+  return(all_permits)
+  
+}
+
+permit_years <- c("2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", 
+                  "2017", "2018", "2019", "2020")
+
+all_permits <- map_df(permit_years, .f = ~append_permit_data(.year = .x))
+
+
+
