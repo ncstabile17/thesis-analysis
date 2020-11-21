57fdd8351b098b1d553c859ffca04329f4a8824f
ncstabile17 <ncstabile17@gmail.com>
2020-11-21 17:40:33 -0500

    Joining 2010 and 2018 rent data, calculating change in rent, mapping

--- descriptive-maps.R
+++ descriptive-maps.R
@@ -16,7 +16,7 @@ census_key <- read_file("data/census_api_key.txt")
 
 census_api_key(census_key, install = TRUE, overwrite = TRUE)
 
-make_acs_map <- function(year, census_var, state_fips, county_fips) {
+make_acs_map <- function(year, census_var, state_fips, county_fips, geometry_flag) {
   
   acs_map_data <- get_acs(
     geography = "tract",
@@ -24,7 +24,7 @@ make_acs_map <- function(year, census_var, state_fips, county_fips) {
     state = state_fips,
     county = county_fips,
     year = year,
-    geometry = TRUE
+    geometry = geometry_flag
   )
   
   acs_map <- ggplot(data = acs_map_data) +
@@ -35,7 +35,29 @@ make_acs_map <- function(year, census_var, state_fips, county_fips) {
   
 }
 
-rent_2018 <- make_acs_map(2018, "B25064_001", 11, 1)
-rent_2010 <- make_acs_map(2010, "B25064_001", 11, 1)
+rent_2018 <- make_acs_map(2018, "B25064_001", 11, 1, FALSE)
+rent_2010 <- make_acs_map(2010, "B25064_001", 11, 1, TRUE)
 
-rent_data_2018 <- rent_2018[[1]]
+rent_data_2018 <- rent_2018[[1]] %>% 
+  rename(
+    estimate_2018 = estimate,
+    moe_2018 = moe
+  ) %>% 
+  select(-NAME, -variable)
+
+rent_data_2010 <- rent_2010[[1]] %>% 
+  rename(
+    estimate_2010 = estimate,
+    moe_2010 = moe
+  )
+
+rent_data_change <- left_join(rent_data_2010, rent_data_2018, by = "GEOID") %>% 
+  mutate(rent_change = (estimate_2018 - estimate_2010)/estimate_2010 * 100)
+
+ggplot(data = rent_data_change) +
+  geom_sf(aes(fill = rent_change)) +
+  theme_void()
+
+ggplot(data = all_permits) +
+  geom_sf(aes(fill = rent_change)) +
+  theme_void()
