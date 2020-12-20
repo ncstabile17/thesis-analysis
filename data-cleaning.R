f007b1d955a95e5fa66310d803f75f56fb8483fc
ncstabile17 <ncstabile17@gmail.com>
2020-12-20 15:01:06 -0500

    Importing and combining DC HMDA data 2007-2017

--- data-cleaning.R
+++ data-cleaning.R
@@ -7,7 +7,7 @@ all_permits <- data.frame()
 append_permit_data <- function(.year) {
   
   file_name <- str_glue(
-    "data/Building_Permits_in_{.year}.csv"
+    "data/permits/Building_Permits_in_{.year}.csv"
   )
   
   new_permit_data <- 
@@ -33,4 +33,39 @@ permit_years <- c("2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016"
 all_permits <- map_df(permit_years, .f = ~append_permit_data(.year = .x))
 
 write.csv(all_permits, "data/all_new_building_permits.csv")
+
+
+# Importing and combining DC HMDA mortgage data
+
+all_hmda <- data.frame()
+
+append_hmda_data <- function(.year) {
+  
+  file_name <- str_glue(
+    "data/hmda/hmda_{.year}_dc_originated-records_labels.csv"
+  )
+  
+  new_hmda_data <- 
+    read_csv(file_name) %>% 
+    rename(year = as_of_year)
+  
+  all_hmda <- bind_rows(
+    all_hmda,
+    new_hmda_data
+  )
+  
+  return(all_hmda)
+  
+}
+
+hmda_years <- c("2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", 
+                  "2017")
+
+all_hmda <- map_df(hmda_years, .f = ~append_hmda_data(.year = .x))
+
+# Removing variables with only NA values
+all_hmda <- all_hmda[ , colSums(is.na(all_hmda)) < nrow(all_hmda)] 
+
+write.csv(all_hmda, "data/all_hmda.csv")
+
 
--- descriptive-maps.R
+++ descriptive-maps.R
@@ -21,7 +21,7 @@ all_new_building_permits <- read_csv("data/all_new_building_permits.csv") %>%
   mutate(address_id = as.character(maraddressrepositoryid)) 
 
 # reading in DC Census tract shape files
-dc_tracts <- st_read("data/Census_Tracts_in_2010.shp") %>% 
+dc_tracts_2010 <- st_read("data/Census_Tracts_in_2010.shp") %>% 
   select(TRACT, GEOID, geometry)
 
 # reading in permits data from Jenny Schuetz
@@ -30,6 +30,7 @@ schuetz_permits <- read_csv("data/tract_permits-GEOID.csv") %>%
   rename(new_permits = 'Permits, new construction',
          new_units = 'Units permitted')
 
+# Custom function to get arbitrary ACS data
 make_acs_map <- function(year, census_var, state_fips, county_fips, geometry_flag) {
   
   acs_map_data <- get_acs(
@@ -49,6 +50,7 @@ make_acs_map <- function(year, census_var, state_fips, county_fips, geometry_fla
   
 }
 
+# Getting ACS data on Median Gross Rent for 2010 and 2018 
 rent_2018 <- make_acs_map(2018, "B25064_001", 11, 1, FALSE)
 rent_2010 <- make_acs_map(2010, "B25064_001", 11, 1, TRUE)
 
@@ -65,6 +67,7 @@ rent_data_2010 <- rent_2010[[1]] %>%
     moe_2010 = moe
   )
 
+# Calculating and mapping change in rent by Census tract
 rent_data_change <- left_join(rent_data_2010, rent_data_2018, by = "GEOID") %>% 
   mutate(rent_change = (estimate_2018 - estimate_2010)/estimate_2010 * 100)
 
@@ -108,21 +111,26 @@ all_new_building_permits <-
 
 all_new_permits_merged <- st_join(
   all_new_building_permits, # points
-  dc_tracts, # polygons
+  dc_tracts_2010, # polygons
   join = st_within
 )
 
 all_new_permits_merged <- st_set_geometry(all_new_permits_merged, NULL)
+
 all_new_permits_by_tract <- all_new_permits_merged %>%
   group_by(GEOID) %>%
   summarize(
     permit_count = n(),
   )
 
-all_new_permits_by_tract <- dc_tracts %>%
+all_new_permits_by_tract <- dc_tracts_2010 %>%
   left_join(all_new_permits_by_tract, by = "GEOID")
 
 # TODO: something looks wrong here, but maybe not?
 ggplot(data = all_new_permits_by_tract) +
   geom_sf(aes(fill = permit_count)) +
   theme_void()
+
+# Working with HMDA data
+
+
